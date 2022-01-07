240f6703ea845b1bef1b39c1c58daa9b5e8b3be7
GabyE77 <94193219+GabyE77@users.noreply.github.com>
2022-01-07 20:33:31 +0100

    Add files via upload

---
+++ runJSprogram/script.js
@@ -0,0 +1,7 @@
+console.log("Hello Winc Academy students")
+// this is a large sum
+/*let multiply = 1230941 * 1823794;
+console.log("multiply:", multiply);
+let devide = 637263 / 54;
+console.log("devide:", devide);
+*/
